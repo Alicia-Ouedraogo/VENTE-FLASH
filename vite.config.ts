e29e3acd45de85f1bd092eeecf9968fb1af9aba1
Alicia Lise Marion <Ouedraogoalicia22@gmail.com>
2025-07-02 03:23:40 +0100

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/VENTE-FLASH/',  // nom du repo GitHub
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   server: {
     hmr: {
-      overlay: false,  // <-- désactive l’overlay d’erreurs esbuild
+      overlay: false,  // désactive l'overlay d'erreurs
     },
   },
 });
